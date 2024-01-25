56dab2ff3bc74a5b559d55ca3f44188480631048
aoyama11 <aoyama1@kenyon.edu>
2024-01-25 08:28:09 -0500

    changed code

--- triangle.rb
+++ triangle.rb
@@ -1,1 +1,22 @@
-puts "hello"
+def get_triangle_type(arr)
+    if arr.length == 3
+        l = arr.uniq.length
+        if l == 1
+            return "equilateral"
+        elsif l == 2
+            return "isoceles"
+        elsif l == 3
+            return "scalene"
+        end
+    else
+        return "not a triangle"
+    end
+end
+
+puts (get_triangle_type([2, 6, 5]))
+
+puts (get_triangle_type([4, 4, 7]))
+    
+puts (get_triangle_type([8, 8, 8]))
+    
+puts (get_triangle_type([3, 5, 5, 2])) 
